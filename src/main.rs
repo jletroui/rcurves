a19298753fc7fed22cf32cd2d9ed5e185c5f39ea
Julien Letrouit <julien.letrouit@gmail.com>
2022-12-04 23:29:35 -0500

    Basic drawing of Lissajou curves

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,51 @@ extern crate opengl_graphics;
 extern crate piston;
 
 use glutin_window::GlutinWindow as Window;
+use graphics::types::Line;
 use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent};
 use piston::window::WindowSettings;
+use std::f64::consts::PI;
+
+pub struct Point {
+    x: f64,
+    y: f64,
+}
+
+impl Point {
+    pub fn new(x: f64, y: f64) -> Self {
+        Self { x, y }
+    }
+
+    pub fn to(self: &Self, other: &Self) -> Line {
+        [self.x, self.y, other.x, other.y]
+    }
+}
+
+struct Lissajou {
+    radius_x: f64,
+    radius_y: f64,
+    a: f64,
+    b: f64,
+    d: f64,
+}
+
+impl Lissajou {
+    pub fn new(radius_x: f64, radius_y: f64, a: f64, b: f64, d: f64) -> Self {
+        Self { radius_x, radius_y, a, b, d }
+    }
+
+    pub fn location(self: &Self, t: f64) -> Point {
+        return Point::new(
+            self.radius_x * f64::sin(self.a * t + self.d),
+            self.radius_y * f64::sin(self.b * t),
+        )
+    }
+}
 
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
-    rotation: f64,  // Rotation for the square.
 }
 
 impl App {
@@ -19,11 +56,12 @@ impl App {
         use graphics::*;
 
         const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+        const RESOLUTION: f64 = 0.01;
+        const TWO_PI: f64 = 2.0 * PI;
 
-        let square = rectangle::square(0.0, 0.0, 50.0);
-        let rotation = self.rotation;
-        let (x, y) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
+        let (rx, ry) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
+        let lissajous = Lissajou::new(rx, ry, 8.0, 9.0, PI / 7.0);
 
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
@@ -31,12 +69,17 @@ impl App {
 
             let transform = c
                 .transform
-                .trans(x, y)
-                .rot_rad(rotation)
-                .trans(-25.0, -25.0);
+                .trans(rx, ry);
 
-            // Draw a box rotating around the middle of the screen.
-            rectangle(RED, square, transform, gl);
+            let mut location = lissajous.location(0.0);
+            let mut t = 0.0;
+
+            while t < TWO_PI {
+                t += RESOLUTION;
+                let new_location = lissajous.location(t);
+                line(WHITE, 1.0, location.to(&new_location), transform, gl);
+                location = new_location;
+            }
         });
     }
 }
@@ -46,7 +89,7 @@ fn main() {
     let opengl = OpenGL::V3_2;
 
     // Create a Glutin window.
-    let mut window: Window = WindowSettings::new("Lissajous", [1920, 1920])
+    let mut window: Window = WindowSettings::new("Lissajous", [1080, 1080])
         .graphics_api(opengl)
         .exit_on_esc(true)
         .build()
@@ -55,7 +98,6 @@ fn main() {
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        rotation: 0.0,
     };
 
     let mut events = Events::new(EventSettings::new());
