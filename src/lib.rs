0fc6f05fadd20aca7ef574d62810ff398000d0a0
Julien Letrouit <julien.letrouit@gmail.com>
2022-12-06 21:45:18 -0500

    End of piston experiment

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,10 @@ mod rendering;
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::{EventSettings, Events};
-use piston::input::RenderEvent;
+use piston::input::{RenderEvent, Button, HatState, ButtonEvent};
+use piston::input::controller::ControllerHat;
 use piston::window::WindowSettings;
-use piston::AdvancedWindow;
+use piston::{AdvancedWindow, ButtonArgs, ButtonState, PressEvent};
 use rendering::Renderer;
 
 pub fn run() {
@@ -22,15 +23,37 @@ pub fn run() {
         .unwrap();
 
     // Create a new game and run it.
-    let mut renderer = Renderer::new(GlGraphics::new(opengl));
-
+    let mut gl = GlGraphics::new(opengl);
+    let mut renderer = Renderer::new(); // TODO: comprendre pourquoi mut nécessaire
     let mut events = Events::new(EventSettings::new());
 
     while let Some(e) = events.next(&mut window) {
         if let Some(args) = e.render_args() {
-            renderer.render(&args);
+            gl.draw(args.viewport(), |c, gl| {
+                renderer.render(&args, c, gl)
+            });
         }
-
-        window.set_title(String::from("Lissajous"));
+        if let Some(button) = e.press_args() {
+            match button {
+                Button::Hat(ControllerHat { id: _, state: HatState::Up, which: _ })=> {
+                    window.set_title(String::from("Up"))
+                }
+                Button::Hat(ControllerHat { id: _, state: HatState::Down, which: _ })=> {
+                    window.set_title(String::from("Down"))
+                }
+                Button::Hat(_) => {
+                    window.set_title(String::from("Hat"))
+                }
+                Button::Controller(_) => {
+                    window.set_title(String::from("Controller"))
+                }
+                Button::Keyboard(_) => {
+                    window.set_title(String::from("Keyboard"))
+                }
+                Button::Mouse(_) => {
+                    window.set_title(String::from("Mouse"))
+                }
+            }
+        }
     }
 }
